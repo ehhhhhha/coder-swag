72e1c263bd46cb3bb092a2a7f28cd936026f1443
aaaa lee <aaaalee1@lalalalu.local>
2018-07-14 21:59:50 +1000

    prototype cell

---
+++ coder-swag/Controller/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCellTableViewCell.swift
+//  coder-swag
+//
+//  Created by aaaa lee on 2018/7/14.
+//  Copyright © 2018年 eha. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
